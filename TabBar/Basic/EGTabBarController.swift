4d080ed194803da5181673b831a48cd6e924d5f8
EG <EG@MacPro.com>
2017-12-21 08:53:09 +0800

    Fix bug

--- TabBar/Basic/EGTabBarController.swift
+++ TabBar/Basic/EGTabBarController.swift
@@ -14,12 +14,6 @@ class EGTabBarController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+     
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
